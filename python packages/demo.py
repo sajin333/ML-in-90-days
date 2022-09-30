b8738458b3161d2ee1f0123b37361ba69b1ce837
sajin333 <sajinmadhavancbe@gmail.com>
2022-09-30 23:45:04 +0100

    new_updates

--- python packages/demo.py
+++ python packages/demo.py
@@ -1,1 +1,12 @@
 print("Hello world")
+print("I am learning about programming and Github")
+import time
+for i in range(0, 10):
+    print(i)
+    time.sleep(1)
+    print(i)
+    time.sleep(1)
+    print(i)
+    time.sleep(1)
+    print(i)
+    time.sleep(1)
